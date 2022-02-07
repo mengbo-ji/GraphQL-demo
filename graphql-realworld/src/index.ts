ae1aa111d3e82afe450ef533f0d3057166d62029
mengbo-ji <jmb33595@163.com>
2022-02-07 16:23:15 +0800

    feat: graphql-realworld-demo

--- graphql-realworld/src/index.ts
+++ graphql-realworld/src/index.ts
@@ -2,36 +2,17 @@
 import * as express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import { typeDefs, resolvers } from './schema'
-import Users from '../data-sources/Users'
-import { User } from '../models'
 
 async function startApolloServer() {
   const app = express();
   const server = new ApolloServer({
     typeDefs,
-    resolvers,
-    // GraphQL 请求都会经过这里
-    // 该函数接收一个对象，Request
-    context: (req) => {
-      return {
-        foo: 'bar', // 返回自定义数据，后续的每个resolvers 都可以获取
-      }
-    },
-    dataSources: () => ({
-      users: new Users(User)
-    })
+    resolvers
   });
 
   await server.start();
 
   server.applyMiddleware({ app });
-
-  app.use((req, res) => {
-    res.status(200);
-    res.send('Hello!');
-    res.end();
-  });
-
   app.listen(1234, () => console.log(`🚀 Server ready at http://localhost:1234${server.graphqlPath}`))
   ;
 }
--- graphql-realworld/src/schema.ts
+++ graphql-realworld/src/schema.ts
@@ -1,41 +1,5 @@
-import { gql } from 'apollo-server-express'
-// 1. 定义 apollo schema
-const typeDefs = gql`
-  type User {
-    _id: ID!
-    name: String!
-    age: Int
-  }
-
-  type Query {
-    users: [ User! ]
-    user(id: ID!): User
-  }
-
-`;
-
-// 2. 定义resolvers
-const resolvers = {
-  Query: {
-    // async users(parent: any, args: any, context: any) {
-    //   const users = await User.find()
-    //   return users
-    // },
-    // async user(parent: any, { id }: any) {
-    //   const user = await User.findById(id)
-    //   return user
-    // }
-    async users(parent: any, args: any, { dataSources }: any) {
-      const users = await dataSources.users.getUsers()
-      return users
-    },
-    async user(parent: any, { id }: any, { dataSources }: any) {
-      const user = await dataSources.users.getUser(id)
-      return user
-    }
-  },
-
-};
+import { typeDefs } from './type-defs'
+import { resolvers } from './resolvers'
 
 export {
   typeDefs,
